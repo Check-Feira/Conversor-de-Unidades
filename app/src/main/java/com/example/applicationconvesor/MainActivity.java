8531e3bb0c016a60b20e5306df26511e5fefa458
Ingridoliveira <oligrid20@gmail.com>
2024-11-06 10:12:15 -0300

    Conversor com arrays de unidades

--- app/src/main/java/com/example/applicationconvesor/MainActivity.java
+++ app/src/main/java/com/example/applicationconvesor/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.applicationconvesor;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Spinner spinner = (Spinner) findViewById(R.id.unidade_imput);
+        Spinner spinner2 = (Spinner) findViewById(R.id.unidade_exit);
+// Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.unidadeInput_array,
+                android.R.layout.simple_spinner_item
+        );
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(
+                this,
+                R.array.unidadeExit_array,
+                android.R.layout.simple_spinner_item
+        );
+// Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner.
+        spinner.setAdapter(adapter);
+        System.out.println(spinner);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
         });
+
+
+
     }
+
+
 }
